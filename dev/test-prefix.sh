855f3cae7f6686406af556769cfd642ae697e6f4
Francis M. Tyers <ftyers@prompsit.com>
2013-08-17 16:02:27 +0000

    committing test prefix

--- dev/test-prefix.sh
+++ dev/test-prefix.sh
@@ -63,4 +63,4 @@ cat $TMP0 | cut -f2 -d':' | $SED 's/^/^/g' | $SED 's/$/$/g' |\
 paste $TMP0 $TMP1 $TMP2 $TMP3 $TMP4
 
 
-#rm $TMPREGEX $TMPOUT $TMP0 $TMP1 $TMP2 $TMP3 $TMP4
+rm $TMPREGEX $TMPFILTER $TMPOUT1 $TMPOUT2 $TMP0 $TMP1 $TMP2 $TMP3 $TMP4
