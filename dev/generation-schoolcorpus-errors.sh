55b62577a691cdfa5ca0fe9c4bf915c2a4247ca8
Trond Trosterud <trond.trosterud@uit.no>
2013-09-02 18:34:39 +0000

    new-texts-new-domain

--- dev/generation-schoolcorpus-errors.sh
+++ dev/generation-schoolcorpus-errors.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CORP=../texts/samediggidiedadus_samegiela_birra_2012.sme.txt
+CORP=../texts/schoolcorpus.sme.txt
 SED=gsed
 TEMPDIREC=/tmp/
 if [[ $TMPDIR != "" ]]; then
@@ -15,15 +15,15 @@ if [[ $1 == "new" ]] ; then
 	cat $TEMPDIREC/sme-sma.postchunk | cut -f2 -d'^' | cut -f1 -d'<' | sed 's/\*//g' | $SED 's/$/./g' | apertium-destxt | hfst-proc -w ../sma-sme.automorf.hfst | apertium-retxt  > $TEMPDIREC/sma.generation
 	cat $TEMPDIREC/sme-sma.postchunk | hfst-proc -d ../sme-sma.autogen.hfst  > $TEMPDIREC/sme-sma.generation
 	cat $TEMPDIREC/sme-sma.generation | sed 's/^ *[0-9]\+ //g' > $TEMPDIREC/sme-sma.result
-	paste $TEMPDIREC/sme-sma.postchunk $TEMPDIREC/sme-sma.result $TEMPDIREC/sma.generation | sed 's/^ *//g' | sed 's/^[0-9]\+/&\t/g' > generation-report.sme-sma.txt
+	paste $TEMPDIREC/sme-sma.postchunk $TEMPDIREC/sme-sma.result $TEMPDIREC/sma.generation | sed 's/^ *//g' | sed 's/^[0-9]\+/&\t/g' > generation-report-schoolcorpus.sme-sma.txt
 else 
-	cat generation-report.sme-sma.txt
+	cat generation-report-schoolcorpus.sme-sma.txt
 fi
 echo ""
-HASH=`cat generation-report.sme-sma.txt | cut -f3 | grep '#' | wc -l`;
-SLASH=`cat generation-report.sme-sma.txt | cut -f3 | grep '\/' | wc -l`;
-AT=`cat generation-report.sme-sma.txt | cut -f3 | grep '@' | wc -l`;
-STAR=`cat generation-report.sme-sma.txt | cut -f3 | grep '*' | wc -l`;
+HASH=`cat generation-report-schoolcorpus.sme-sma.txt | cut -f3 | grep '#' | wc -l`;
+SLASH=`cat generation-report-schoolcorpus.sme-sma.txt | cut -f3 | grep '\/' | wc -l`;
+AT=`cat generation-report-schoolcorpus.sme-sma.txt | cut -f3 | grep '@' | wc -l`;
+STAR=`cat generation-report-schoolcorpus.sme-sma.txt | cut -f3 | grep '*' | wc -l`;
 
 echo $HASH | sed 's/^/ # /g'
 echo $SLASH | sed 's/^/ \/ /g'
@@ -32,5 +32,5 @@ echo $STAR | sed 's/^/ * /g'
 
 if [[ $1 == "new" ]]; then 
 	DATE=`date +"%Y-%m-%d %H:%M:%S"`;
-	printf "$DATE\t$HASH\t$SLASH\t$AT\t$STAR\n" >> generation-history.log
+	printf "$DATE\t$HASH\t$SLASH\t$AT\t$STAR\n" >> generation-history-schoolcorpus.log
 fi
