59a2c0b77b00460f456762679563afb5d587cbed
Trond Trosterud <trond.trosterud@uit.no>
2016-04-28 12:56:11 +0000

    gsed but printf

--- dev/bidix-sanity.sh
+++ dev/bidix-sanity.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 
 pair=`pwd | grep -o 'apertium-[a-z][a-z][a-z]-[a-z][a-z][a-z]'`;
-dir=`echo $pair | grep -o  -- '-[a-z][a-z][a-z]-[a-z][a-z][a-z]' | sed 's/^-//g'`; 
+dir=`printf "%s\n" "$pair" | grep -o  -- '-[a-z][a-z][a-z]-[a-z][a-z][a-z]' | gsed 's/^-//g'`; 
 dix=$pair.$dir.dix
-lang2=`echo $dir | cut -f2 -d'-'`
-gtdir=`echo $GTCORE | sed 's/\(gtcore\|core\)//g'`;
-analysator="/Users/mka055/Documents/langtechsvn/gtsvn/langs/sma/tools/mt/apertium/analyser-mt-apertium-desc.und.hfstol"
+lang2=`printf "%s\n"  "$dir" | cut -f2 -d'-'`
+gtdir=`printf "%s\n"  "$GTCORE" | gsed 's/\(gtcore\|core\)//g'`;
+analysator=$gtdir"/langs/"$lang2"/tools/mt/apertium/analyser-mt-apertium-desc.und.hfstol"
 
-lt-expand ../$dix | sed 's/\(<[^>]\+>\)\(<[^>]\+>\)\+/\1/g' | sed 's/:[><]:/:/g'  | grep -v ':\([[:punct:]]\|[[:space:]]\)' | grep -v -- '-<' | grep -v '\/' | sort -u > /tmp/$dir.exp
+lt-expand ../$dix | gsed 's/\(<[^>]\+>\)\(<[^>]\+>\)\+/\1/g' | gsed 's/:[><]:/:/g'  | grep -v ':\([[:punct:]]\|[[:space:]]\)' | grep -v -- '-<' | grep -v '\/' | sort -u > /tmp/$dir.exp
 
 cat /tmp/$dir.exp | cut -f2- -d':' | cut -f1 -d'<' | hfst-proc -w $analysator > /tmp/$dir.a
 
 wc -l /tmp/$dir.exp /tmp/$dir.a
 
-for line in `paste /tmp/$dir.exp /tmp/$dir.a  | sed 's/\t/:/g' | sed 's/ /¬/g'`; do
-	lang1s=`echo $line | cut -f1 -d':'`;
-	lang2s=`echo $line | cut -f2 -d':'`;
-	lang2m=`echo $line | cut -f3 -d':'`;
+for line in `paste /tmp/$dir.exp /tmp/$dir.a  | gsed 's/\t/:/g' | gsed 's/ /¬/g'`; do
+	lang1s=`printf "%s\n"  "$line" | cut -f1 -d':'`;
+	lang2s=`printf "%s\n"  "$line" | cut -f2 -d':'`;
+	lang2m=`printf "%s\n"  "$line" | cut -f3 -d':'`;
 
-	num=`echo "$lang2m" | grep -o "$lang2s" | wc -l`
-#	echo $num" "$lang2m" "$lang2s;
+	num=`printf "%s\n"   "$lang2m" | grep -o "$lang2s" | wc -l`
+#	gecho $num" "$lang2m" "$lang2s;
 	if [[ $num -eq 0 ]]; then
-		echo -e "%\t"$line;
+		printf "%%\t%s\n" "$line";
 	else
-#		echo -e "+\t"$line;
+#		printf "%%\t%s\n" "$line";
 		continue
 	fi
 done
 	
-
