ac73ada29695367ac491655279643417af333459
Lene Antonsen <lene.antonsen@uit.no>
2016-04-28 11:25:44 +0000

    sed instead of gsed

--- dev/bidix-sanity.sh
+++ dev/bidix-sanity.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 pair=`pwd | grep -o 'apertium-[a-z][a-z][a-z]-[a-z][a-z][a-z]'`;
-dir=`echo $pair | grep -o  -- '-[a-z][a-z][a-z]-[a-z][a-z][a-z]' | gsed 's/^-//g'`; 
+dir=`echo $pair | grep -o  -- '-[a-z][a-z][a-z]-[a-z][a-z][a-z]' | sed 's/^-//g'`; 
 dix=$pair.$dir.dix
 lang2=`echo $dir | cut -f2 -d'-'`
-gtdir=`echo $GTCORE | gsed 's/\(gtcore\|core\)//g'`;
+gtdir=`echo $GTCORE | sed 's/\(gtcore\|core\)//g'`;
 analysator=$gtdir"/langs/"$lang2"/tools/mt/apertium/analyser-mt-apertium-desc.und.hfstol"
 
-lt-expand ../$dix | gsed 's/\(<[^>]\+>\)\(<[^>]\+>\)\+/\1/g' | gsed 's/:[><]:/:/g'  | grep -v ':\([[:punct:]]\|[[:space:]]\)' | grep -v -- '-<' | grep -v '\/' | sort -u > /tmp/$dir.exp
+lt-expand ../$dix | sed 's/\(<[^>]\+>\)\(<[^>]\+>\)\+/\1/g' | sed 's/:[><]:/:/g'  | grep -v ':\([[:punct:]]\|[[:space:]]\)' | grep -v -- '-<' | grep -v '\/' | sort -u > /tmp/$dir.exp
 
 cat /tmp/$dir.exp | cut -f2- -d':' | cut -f1 -d'<' | hfst-proc -w $analysator > /tmp/$dir.a
 
 wc -l /tmp/$dir.exp /tmp/$dir.a
 
-for line in `paste /tmp/$dir.exp /tmp/$dir.a  | gsed 's/\t/:/g' | gsed 's/ /¬/g'`; do
+for line in `paste /tmp/$dir.exp /tmp/$dir.a  | sed 's/\t/:/g' | sed 's/ /¬/g'`; do
 	lang1s=`echo $line | cut -f1 -d':'`;
 	lang2s=`echo $line | cut -f2 -d':'`;
 	lang2m=`echo $line | cut -f3 -d':'`;
