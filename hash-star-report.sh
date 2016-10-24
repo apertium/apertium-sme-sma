11bd3312304f051603546c2af31c28bb8f8dbff1
Trond Trosterud <trond.trosterud@uit.no>
2016-10-24 14:57:57 +0000

    sma: No need to do without Finnish loan words for sma.

--- hash-star-report.sh
+++ hash-star-report.sh
@@ -12,11 +12,11 @@ hashunique=`cat /tmp/hashall|sort|uniq|wc -l`
 
 cat /tmp/hashall|sort|uniq -c|sort -nr > dev/hash-report.freq 
 
-cat /tmp/hashstarall|grep '^\*'|cut -c2-|tr -d '[.,:;)(?!]'|grep -v xxx|lookup -q /Users/trond/main/langs/fin/src/analyser-gt-desc.xfst|grep '?'|cut -f1 > /tmp/starallnofinnish
+#cat /tmp/hashstarall|grep '^\*'|cut -c2-|tr -d '[.,:;)(?!]'|grep -v xxx|lookup -q /Users/trond/main/langs/fin/src/analyser-gt-desc.xfst|grep '?'|cut -f1 > /tmp/starallnofinnish
 cat /tmp/hashstarall|grep '^\*'|cut -c2-|tr -d '[.,:;)(?!]'|grep -v xxx|cut -f1 > /tmp/starall
 
 starall=`cat /tmp/starall|wc -l `
-starallnofinnish=`cat /tmp/starallnofinnish|wc -l `
+#starallnofinnish=`cat /tmp/starallnofinnish|wc -l `
 
 cat /tmp/starall|sort|uniq -c|sort -nr > dev/star-report.freq
 
@@ -28,11 +28,11 @@ cat /tmp/starall|sort|uniq -c|sort -nr > dev/star-report.freq
 
 hashpercentage=`echo 3k $hashall $all /p | dc`
 starpercentage=`echo 3k $starall $all /p | dc`
-starpercentagenofinnish=`echo 3k $starallnofinnish $all /p | dc`
+#starpercentagenofinnish=`echo 3k $starallnofinnish $all /p | dc`
 
 hashpercentageh=`echo 3k $hashpercentage 100 *p | dc`
 starpercentageh=`echo 3k $starpercentage 100 *p | dc`
-starpercentagehnofinnish=`echo 3k $starpercentagenofinnish 100 *p | dc`
+#starpercentagehnofinnish=`echo 3k $starpercentagenofinnish 100 *p | dc`
 
 echo ""
 echo "Dáppe leat otpt_dir-fiillaid star/hash-raporttat"
@@ -45,9 +45,9 @@ echo "     $hashall hash-sáni (#)"
 echo ""
 echo "hash-proseanta lea $hashpercentageh %"
 echo "star-proseanta (buot sánit) lea $starpercentageh % "
-if [ $(echo $starpercentagehnofinnish) != "0" ] ; then
-    echo "star-proseanta (suomagiel sániid haga) lea $starpercentagehnofinnish % "
-    fi
+#if [ $(echo $starpercentagehnofinnish) != "0" ] ; then
+#    echo "star-proseanta (suomagiel sániid haga) lea $starpercentagehnofinnish % "
+#    fi
 echo ""
 
 
