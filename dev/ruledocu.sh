8712075519a62b5075e48f6c0fbb9df9bc39e17c
Lene Antonsen <lene.antonsen@uit.no>
2016-11-25 16:04:21 +0000

    script to make documentation for the rules. Macroes are next step.

---
+++ dev/ruledocu.sh
@@ -0,0 +1,36 @@
+
+grep 'rule c.*\[' apertium-sme-sma.sme-sma.t1x | grep -v '^<\!' |cut -d '"' -f2 |sed 's/\[ \]/\[_\]/'  > rules
+echo 'Rules for verbals:' > doculist
+grep '\-V ' rules |rev | sort | rev >> doculist
+echo ' ' >> doculist
+echo 'Rules for predicates:' >> doculist
+grep '\-PRED ' rules |rev | sort | rev >> doculist
+echo ' ' >> doculist
+echo 'Rules for adjectives:' >> doculist
+grep '\-A ' rules |rev | sort | rev >> doculist
+echo ' ' >> doculist
+echo 'Rules for bare nouns:' >> doculist
+grep '\-N ' rules |rev | sort | rev >> doculist
+echo ' ' >> doculist
+echo 'Rules for noun phrases:' >> doculist
+grep '\-NP ' rules |rev | sort | rev >> doculist
+echo ' ' >> doculist
+echo 'Rules for numeral expressions:' >> doculist
+grep '\-NUM ' rules |rev | sort | rev >> doculist
+echo ' ' >> doculist
+echo 'Rules for adverbs:' >> doculist
+grep '\-ADV ' rules |rev | sort | rev >> doculist
+echo ' ' >> doculist
+echo 'Rules for P phrases:' >> doculist
+grep '\-PP ' rules |rev | sort | rev >> doculist
+echo ' ' >> doculist
+echo 'Rules for other phrases:' >> doculist
+grep '\-PHR ' rules |rev | sort | rev >> doculist
+echo ' ' >> doculist
+echo 'Small word classes:' >> doculist
+grep '\-CC ' rules |rev | sort | rev >> doculist
+grep '\-CS ' rules |rev | sort | rev >> doculist
+grep '\-IJ ' rules |rev | sort | rev >> doculist
+echo ' ' >> doculist
+echo 'Technical things:' >> doculist
+grep '\-T ' rules |rev | sort | rev >> doculist
